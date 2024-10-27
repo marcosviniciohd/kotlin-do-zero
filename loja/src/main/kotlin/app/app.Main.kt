1f7a40127db0e68fea71727fcf45407fce44f7c5
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-10-26 21:41:53 -0300

    chore: commit inicial

---
+++ loja/src/main/kotlin/app/app.Main.kt
@@ -0,0 +1,9 @@
+package app
+
+import vitrine.Produto
+import carrinho.Produto as carrinho
+
+fun main() {
+    val cadProdutoVitrine = Produto(1, "Camiseta", 39.90)
+    val cadProdutoCarrinho = carrinho(1, 39.90, 1)
+}
